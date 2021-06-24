49976b6ee5dbdb11b9e1acf818379a29600068f6
akvilary <82627635+akvilary@users.noreply.github.com>
2021-06-24 20:24:20 +0700

    Add files via upload

---
+++ Email_merging/main.py
@@ -0,0 +1,21 @@
+#TODO: Create a letter using starting_letter.txt
+with open("./Input/Letters/starting_letter.txt") as letterfile:
+    letter = letterfile.read()
+
+with open("./Input/Names/invited_names.txt") as namesfile:
+    names = namesfile.readlines()
+
+for name in names:
+    new_name = name.strip()
+    new_letter = letter.replace("[name]", new_name)
+    with open(f"./Output/ReadyToSend/letter_for_{new_name}.txt", mode="w") as nameletter:
+        nameletter.write(new_letter)
+
+
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
